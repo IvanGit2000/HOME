c948d66351989b3b79fd2837f6b338a9a759267d
PythonHubStudio <example@mail.com>
2024-07-05 13:14:42 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,20 +1,19 @@
-from importlib.resources import contents
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     context = {
-        'title': 'home',
-        'content': 'Main page store = HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False,
+        'title': 'Home',
+        'content': 'Furniture store HOME',
     }
-    
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'About',
+        'content': 'About us',
+        'text_on_page': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur.',
+    }
+    return render(request, 'main/about.html', context)
 
