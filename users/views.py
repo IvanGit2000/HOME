f48df3f97a36f708c85f94e0e2c4d65774afd032
PythonHubStudio <example@mail.com>
2024-07-28 16:13:14 +0300

    login required decorator - redirect to target page

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request=request, message=f"Welcome {username}!!!")
+                
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next')) 
+                
                 return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginform()
